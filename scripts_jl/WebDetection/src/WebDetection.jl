102343fcac5e8adffa2d3d3b7f7615165271a98c
Hertkorn, Georg <georg.hertkorn@adwmainz.de>
2025-09-16 14:47:09 +0200

    Query Wikidata for p31, p279, p136, p921 separately

--- scripts_jl/WebDetection/src/WebDetection.jl
+++ scripts_jl/WebDetection/src/WebDetection.jl
@@ -115,6 +115,78 @@ function get_wikidata_by_freebase_id(freebase_id::String; language::String="de")
 end
 
 """
+    get_wikidata_by_freebase_id(entity_id::String, property::String; language::String="de") -> Vector{Dict{String, Any}}
+
+Query the Wikidata SPARQL-API for a freebase id (P646) or a Google Knowledge Graph id (P2671).
+Retrieve IDs and labels for `property`
+
+# Arguments
+- `entity_id::String`: Freebase id or Google Knowledge Graph id.
+- `property`: e.g. "P31"
+- `language::String`: Language for the labels (default: "de").
+
+# Returns
+- A vector of tuples:
+    - entityId: value of entity_id
+    - entityWd: Wikidata ID of the entity
+    - entityWdLabel: Label of the entity
+    - `property`: Wikidata ID of the property value
+    - `property` * Label : property value label
+"""
+function get_wikidata_by_freebase_id(entity_id::String, property::String; language::String="de")    
+    pb = lowercasefirst(property)
+    wdtP = uppercasefirst(property)
+    query = """
+    SELECT DISTINCT ?entityWd ?entityWdLabel ?$pb ?$(pb)Label
+    WHERE {
+      {
+      ?entityWd wdt:P646 "$entity_id" .
+      }
+      UNION
+      {
+      ?entityWd wdt:P2671 "$entity_id" .
+      }
+      OPTIONAL {
+        ?entityWd wdt:$wdtP ?$pb .
+        ?$pb rdfs:label ?$(pb)Label .
+        FILTER(LANG(?$(pb)Label) = "$language")
+      }      
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "$language" . }
+    }    
+    """
+
+    url = "https://query.wikidata.org/sparql"
+    headers = ["Accept" => "application/sparql-results+json"]
+
+    function bindings_to_tuple(bindings) 
+        results = []
+        for binding in bindings
+            if !haskey(binding, pb)
+                continue
+            end
+            key_list = ("entityWd", "entityWdLabel", pb, pb * "Label")            
+            value_tuple = NamedTuple((Symbol(k) => binding[k]["value"] for k in key_list))
+            push!(results, NamedTuple((entityId = entity_id, value_tuple...)))
+        end
+        return results
+    end
+
+    try
+        response = HTTP.get(url, query=Dict("query" => query), headers)
+        if response.status == 200
+            data = JSON3.read(response.body)            
+            return bindings_to_tuple(data.results.bindings)
+        else
+            @warn "Error during the query: $(response.status)"
+            return []
+        end
+    catch e
+        @warn "Exception during the query: $e"
+        return []
+    end
+end
+
+"""
     collect_wd_classes(web_entities::String[])
 
 Query Wikidata and collect classes for each item in `web_entities`.
@@ -146,6 +218,42 @@ function collect_wd_classes(web_entitities::Vector{String}, language="en")
                 end                
                 push!(df, (web_entity, wd_id, wd_result["label"], class))
             end          
+        end
+    end
+    return df
+end
+
+"""
+    collect_wd_property(web_entities::String[])
+
+Query Wikidata and collect classes for each item in `web_entities`.
+    
+# Returns
+- data frame with web entity ID, Wikidata ID, Wikidata label, class label
+"""
+function collect_wd_property(web_entitities::Vector{String}, property::String; language="en", limit = 10000)
+    pb = lowercasefirst(property)
+    df = DataFrame(
+        :entityId => String[],
+        :entityWd => String[],
+        :entityWdLabel => String[],
+        Symbol(pb) => String[],
+        Symbol(pb * "Label") => String[]
+    )
+    unique_web_entities = unique(web_entitities)
+    msg_interval = 50
+    counter = 0
+    for web_entity in unique_web_entities
+        counter += 1
+        if counter % msg_interval == 0
+            @info counter
+        end
+        if counter > limit
+            break
+        end
+        wd_result_set = get_wikidata_by_freebase_id(web_entity, property, language=language)
+        for wd_result in wd_result_set            
+            push!(df, wd_result)
         end
     end
     return df
--- scripts_jl/WebDetection/src/run.jl
+++ scripts_jl/WebDetection/src/run.jl
@@ -1,5 +1,13 @@
+# 2025-09-16 init package
+# do this only once after you cloned the repository
+using Pkg
+repo_root = "./Documents/projects/imgnetmkr"
+Pkg.activate(joinpath(repo_root, "scripts_jl/WebDetection/"))
+Pkg.instantiate()
+
+# 2025-09-16 start your session
 using Revise, Pkg
-repo_root = "./Documents/projects/graphim"
+repo_root = "./Documents/projects/imgnetmkr"
 Pkg.activate(joinpath(repo_root, "scripts_jl/WebDetection/"))
 using DataFrames
 using WebDetection
@@ -48,3 +56,72 @@ df_wd_class_count = combine(gdf, nrow)
 sort!(df_wd_class_count, :nrow, rev=true)
 
 CSV.write("./data/di-100/counts/wd_class_count_en.csv", df_wd_class_count)
+
+# 2029-09-16 get also IDs for the classes
+using Revise, Pkg
+repo_root = "./Documents/projects/imgnetmkr"
+Pkg.activate(joinpath(repo_root, "scripts_jl/WebDetection/"))
+using DataFrames
+using CSV
+using WebDetection
+
+# get df_file_web_entities and web_entities: see above
+
+# tests
+# fb_id = "/m/02csf"
+fb_id = "/m/080kk2c"
+qr = WebDetection.get_wikidata_by_freebase_id(fb_id, "p31")
+
+# get data for P31: instance of
+
+df_p31 = WebDetection.collect_wd_property(web_entities, "P31", language = "de")
+CSV.write("./data/di-100/counts/web_entity-p31.csv", df_p31, delimiter = ",")
+
+df_file_p31 = innerjoin(df_file_web_entities, df_p31[:, [:entityId, :entityWd, :p31, :p31Label]], on = :web_entity => :entityId)
+
+gdf = groupby(df_file_p31, [:p31, :p31Label])
+df_p31_count = combine(gdf, nrow)
+sort!(df_p31_count, :nrow, rev=true)
+
+CSV.write("./data/di-100/counts/p31_filecount_de.csv", df_p31_count)
+
+# get data for P279: subclass of
+
+df_p279 = WebDetection.collect_wd_property(web_entities, "P279", language = "de")
+CSV.write("./data/di-100/counts/web_entity-p279.csv", df_p279, delimiter = ",")
+
+df_file_p279 = innerjoin(df_file_web_entities, df_p279[:, [:entityId, :entityWd, :p279, :p279Label]], on = :web_entity => :entityId)
+
+gdf = groupby(df_file_p279, [:p279, :p279Label])
+df_p279_count = combine(gdf, nrow)
+sort!(df_p279_count, :nrow, rev=true)
+
+CSV.write("./data/di-100/counts/p279_filecount_de.csv", df_p279_count)
+
+# get data for P136: genre
+
+df_p136 = WebDetection.collect_wd_property(web_entities, "P136", language = "de")
+CSV.write("./data/di-100/counts/web_entity-p136.csv", df_p136, delimiter = ",")
+
+df_file_p136 = innerjoin(df_file_web_entities, df_p136[:, [:entityId, :entityWd, :p136, :p136Label]], on = :web_entity => :entityId)
+
+gdf = groupby(df_file_p136, [:p136, :p136Label])
+df_p136_count = combine(gdf, nrow)
+sort!(df_p136_count, :nrow, rev=true)
+
+CSV.write("./data/di-100/counts/p136_filecount_de.csv", df_p136_count)
+
+# get data for P921: main subject
+
+df_p921 = WebDetection.collect_wd_property(web_entities, "P921", language = "de")
+CSV.write("./data/di-100/counts/web_entity-p921.csv", df_p921, delimiter = ",")
+
+df_file_p921 = innerjoin(df_file_web_entities, df_p921[:, [:entityId, :entityWd, :p921, :p921Label]], on = :web_entity => :entityId)
+
+gdf = groupby(df_file_p921, [:p921, :p921Label])
+df_p921_count = combine(gdf, nrow)
+sort!(df_p921_count, :nrow, rev=true)
+
+CSV.write("./data/di-100/counts/p921_filecount_de.csv", df_p921_count)
+
+
